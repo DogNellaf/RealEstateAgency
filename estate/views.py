2fb0be7e3012c38388261ec12aaf090a377a5748
FallenGod <danil.sidorov16@gmail.com>
2021-06-16 00:39:59 +0300

    New design

--- estate/views.py
+++ estate/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Стартовая страница
 def index(request):
     return render(request, 'index.html')
+
+def contacts(request):
+    return render(request, 'contacts.html')
