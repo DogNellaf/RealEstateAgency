eda14893cc76fdf8041a5357bfe96e84ae9de661
FallenGod <danil.sidorov16@gmail.com>
2021-06-16 23:23:33 +0300

    Index, about, contacts, catalog fix

--- estate/urls.py
+++ estate/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('contacts', views.contacts, name='contacts'),
+    path('about', views.about, name='about'),
+    path('catalog', views.catalog, name='catalog'),
+    path('services', views.services, name='services'),
+    path('jobopenings', views.jobopenings, name='jobopenings'),
 ]
--- estate/views.py
+++ estate/views.py
@@ -6,3 +6,15 @@ def index(request):
 
 def contacts(request):
     return render(request, 'contacts.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def catalog(request):
+    return render(request, 'catalog.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def jobopenings(request):
+    return render(request, 'jobopening.html')
